ff0070d9f6102ad6e28e3f01cd01240d2a1e3e73
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-03-18 11:53:11 +0800

    Routing in Laravel
    To run server -> php artisan serve\nTo see route list -> php artisan route:list\nRouting changes are done in /routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Routings happen here
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Main Page";
 });
+
+Route::get("/hello", function () {
+    return "Hello";
+})->name("hello"); // This is route naming
+
+// Redirect URL
+Route::get("/hallo", function () {
+    return redirect()->route("hello"); // Redirecting using route name
+});
+
+// For value specific routes (the arguments will be cached)
+Route::get("/greet/{name}", function ($name) {
+    return "Hello " . $name . "!";
+});
+
+// If the route is not defined
+Route::fallback(function () {
+    return "Still got somewhere!";
+});
+
+// GET
+// POST
+// PUT
+// DELETE
