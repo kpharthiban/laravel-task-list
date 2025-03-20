6c081479794e69ed444578e5ac86ff82a1310689
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-03-20 18:41:13 +0800

    Addind tailwind css and doing basic stylings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
 
 <head>
     <title>Laravel 12 Task List App</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     @yield('styles')
 </head>
 
-<body>
-    <h1>@yield("title")</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-2xl">@yield("title")</h1>
     <div>
         {{-- Flash message --}}
         @if (session()->has('success'))
